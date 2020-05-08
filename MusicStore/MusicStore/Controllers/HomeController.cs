a965f241ce9769545c4799a232f9749efc81c44a
Mihui <mihai.anghelin@gmail.com>
2020-05-08 20:33:45 +0300

    Configured the connection to the database on the server

---
+++ MusicStore/MusicStore/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using System.Diagnostics;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using MusicStore.Models;
+
+namespace MusicStore.Controllers
+{
+
+
+    public class HomeController : Controller
+    {
+        private readonly MusicStoreContext _context = new MusicStoreContext();
+
+        public IActionResult Index()
+        {
+            var guitars = _context.Guitar.ToList();
+
+            return View(guitars);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ MusicStore/MusicStore/Models/MusicStoreContext.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace MusicStore.Models
+{
+    public partial class MusicStoreContext : DbContext
+    {
+        public MusicStoreContext()
+        {
+        }
+
+        public MusicStoreContext(DbContextOptions<MusicStoreContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Guitar> Guitar { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
+                optionsBuilder.UseMySql("server=167.71.46.107; database=MusicStore; user=admin; Pwd=GreenMood2020!;");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Guitar>(entity =>
+            {
+                entity.Property(e => e.Id).HasColumnType("int(11)");
+
+                entity.Property(e => e.Description).HasColumnType("varchar(45)");
+
+                entity.Property(e => e.Name)
+                    .IsRequired()
+                    .HasColumnType("varchar(45)");
+            });
+        }
+    }
+}
